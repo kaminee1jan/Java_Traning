798aca796724b0353eec76fea2b7ff7e07cd57c4
KESANG_SHAKYA <83804073+kaminee1jan@users.noreply.github.com>
2022-11-19 22:06:15 -0800

    Add files via upload

---
+++ stringobject.java
@@ -0,0 +1,14 @@
+package stringobject;
+
+public class stringobject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="javaprogramming";
+		String value=new String("Hello");
+		System.out.println(name);
+		System.out.println(value);
+
+	}
+
+}
