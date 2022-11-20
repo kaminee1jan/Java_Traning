91241281323a2c09acfa1d0019dec96dcf0c5b75
KESANG_SHAKYA <83804073+kaminee1jan@users.noreply.github.com>
2022-11-19 22:00:34 -0800

    Add files via upload

---
+++ inputuser.java
@@ -0,0 +1,14 @@
+package inputuser;
+import java.util.Scanner;
+public class inputuser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("what is your name");
+		Scanner sub=new Scanner(System.in);
+		String name=sub.next();
+		System.out.print(name);
+		
+	}
+
+}
