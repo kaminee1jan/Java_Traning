5580774fd235fb619b0abd9cfa131f40886856d9
KESANG_SHAKYA <83804073+kaminee1jan@users.noreply.github.com>
2022-11-19 21:59:00 -0800

    Add files via upload

---
+++ inheritenceExample.java
@@ -0,0 +1,14 @@
+package inheritenceExample;
+
+public class inheritenceExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog labrador=new Dog();
+		labrador.name="ruby";
+		labrador.eat();
+		labrador.bark();
+		System.out.println(labrador.name);
+	}
+
+}
