b04bcace8ec666f7deed8579c883fb08b408b10e
KESANG_SHAKYA <83804073+kaminee1jan@users.noreply.github.com>
2022-11-19 22:02:06 -0800

    Add files via upload

---
+++ SecondInheritence.java
@@ -0,0 +1,18 @@
+package SecondInheritence;
+
+public class SecondInheritence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child whiterose =new Child();
+		whiterose.color="white";
+		whiterose.lifespan=7;
+		whiterose.thorns=13;
+		whiterose.fragrence();
+		whiterose.type();
+		System.out.println(whiterose.lifespan);
+	}
+
+}
+
+
