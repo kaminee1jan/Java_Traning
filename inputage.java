e985d8e16ab95f4e4bd35b7cbde89846aa2f7cd9
KESANG_SHAKYA <83804073+kaminee1jan@users.noreply.github.com>
2022-11-19 21:59:41 -0800

    Add files via upload

---
+++ inputage.java
@@ -0,0 +1,13 @@
+package inputage;
+import java.util.Scanner;
+public class inputage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter your age....");
+		Scanner sub=new Scanner(System.in);
+		int age=sub.nextInt();
+		System.out.print("My age "+age);
+	}
+
+}
