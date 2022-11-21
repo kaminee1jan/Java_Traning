9f75e51212e19b5809418c4ae6f8c8bf24571939
KESANG_SHAKYA <83804073+kaminee1jan@users.noreply.github.com>
2022-11-21 09:01:32 -0800

    Add files via upload

---
+++ ArrayObject.java
@@ -0,0 +1,24 @@
+package ArrayObject;
+
+public class ArrayObject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num[]= {1,2,3,4,6,8,5};
+		String value[]= {"Hello","World","Hi","java","HTML"};
+		char data[]= {'a','9','@','+','*','#','&','%'};
+		for(int i=0; i<num.length;i++) {
+			System.out.println(num[i]);
+		}
+		for(int i=0; i<value.length;i++) {
+			System.out.println(value[i]);
+		}
+		for(int i=0; i<data.length;i++) {
+			System.out.println(data[i]);
+		}
+//		
+
+	}
+
+}
+
