7a5ed618b14b872741e89cb5253e382163c92b98
KESANG_SHAKYA <83804073+kaminee1jan@users.noreply.github.com>
2022-12-09 21:18:11 -0800

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,32 @@
+package TypeCasting;
+import java.util.Scanner;
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter the string....");
+		Scanner sub=new Scanner(System.in);
+		system.out.println(sub.reverse()); 
+//		String[][] names =new String[2][2];
+//		names[0][0]= "23456";
+//		String nam1="Java";
+//		String nam2=nam1+"Programming";
+////		int num Integer.parseInt(names[0][0]);
+//		String value=names[0][0]+2;
+//		System.out.println(value);
+//		System.out.println(nam2);
+//		String[] names= {"java","programmin","python"};
+//		for (String name:names) {
+//			System.out.println(name);
+//		}
+//		int[] numbers= {1,2,3,4,5,6,7,8,9};
+////		advanced for loop for printing the values
+//		for(int num:numbers)
+//		{
+//			System.out.println(num);
+//		}
+
+	}
+
+}
