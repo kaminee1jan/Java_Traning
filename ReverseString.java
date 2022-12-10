bcb719dc13efe192ac2220f303c5f566d97345b3
KESANG_SHAKYA <83804073+kaminee1jan@users.noreply.github.com>
2022-12-09 21:15:41 -0800

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,22 @@
+package Reverse;
+import java.util.Scanner;
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+		Scanner sc=new Scanner(System.in);
+		name=sc.next();
+		int length=name.length();
+		System.out.println(length);
+		String newname=" ";
+		for(int i=length-1; i>=0; i--)
+		{
+			char value=name.charAt(i);
+			newname=newname+value;
+		}
+		System.out.println(newname);
+		
+	}
+
+}
